9ea4cbf1bb63c78548195520a2a87d47ed7c7547
Nikhil <nikhil13012000@gmail.com>
2020-04-12 00:42:57 +0530

    sliding added

--- content-script.js
+++ content-script.js
@@ -1,18 +1,17 @@
 chrome.runtime.onMessage.addListener(function(msg, sender) {
     if (msg == "toggle") {
         toggle();
-        console.log(msg);
     }
 })
 
 var iframe = document.createElement('iframe');
-
+iframe.style.background = "green";
 iframe.style.height = "100%";
 iframe.style.width = "0px";
 iframe.style.position = "fixed";
 iframe.style.top = "0px";
 iframe.style.right = "0px";
-iframe.style.zIndex = "1000";
+iframe.style.zIndex = "9000000000000000000";
 iframe.frameBorder = "none";
 iframe.src = chrome.extension.getURL("popup.html")
 
@@ -20,7 +19,19 @@ document.body.appendChild(iframe);
 
 function toggle() {
     if (iframe.style.width == "0px") {
-        iframe.style.width = "300px";
+        let fwidth = 0;
+        var id = setInterval(frame, 10);
+
+        function frame() {
+            if (fwidth === 310) {
+                clearInterval(id);
+            } else {
+                fwidth += 10;
+                console.log(iframe.style.width);
+                iframe.style.width = fwidth + "px";
+            }
+        }
+
     } else {
         iframe.style.width = "0px";
     }
