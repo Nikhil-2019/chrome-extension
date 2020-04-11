9742b8c44e5f33c8023b122fd0b4400db5cffaf2
Nikhil <nikhil13012000@gmail.com>
2020-04-12 01:10:16 +0530

    slider transition added

--- content-script.js
+++ content-script.js
@@ -15,24 +15,16 @@ iframe.style.zIndex = "9000000000000000000";
 iframe.frameBorder = "none";
 iframe.src = chrome.extension.getURL("popup.html")
 
+
 document.body.appendChild(iframe);
 
 function toggle() {
     if (iframe.style.width == "0px") {
-        let fwidth = 0;
-        var id = setInterval(frame, 10);
-
-        function frame() {
-            if (fwidth === 310) {
-                clearInterval(id);
-            } else {
-                fwidth += 10;
-                console.log(iframe.style.width);
-                iframe.style.width = fwidth + "px";
-            }
-        }
+        iframe.style.width = '300px'
+        iframe.style.transition = "width 1s"
 
     } else {
         iframe.style.width = "0px";
+        iframe.style.transition = "width 1s"
     }
 }
