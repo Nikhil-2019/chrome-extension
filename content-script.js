465f8b68ddb8758ad4e1f8ee395bac92192255b7
Nikhil <nikhil13012000@gmail.com>
2020-04-11 23:31:25 +0530

    frame set

--- content-script.js
+++ content-script.js
@@ -6,13 +6,13 @@ chrome.runtime.onMessage.addListener(function(msg, sender) {
 })
 
 var iframe = document.createElement('iframe');
-iframe.style.background = "green";
+
 iframe.style.height = "100%";
 iframe.style.width = "0px";
 iframe.style.position = "fixed";
 iframe.style.top = "0px";
 iframe.style.right = "0px";
-iframe.style.zIndex = "9000000000000000000";
+iframe.style.zIndex = "1000";
 iframe.frameBorder = "none";
 iframe.src = chrome.extension.getURL("popup.html")
 
@@ -20,7 +20,7 @@ document.body.appendChild(iframe);
 
 function toggle() {
     if (iframe.style.width == "0px") {
-        iframe.style.width = "400px";
+        iframe.style.width = "300px";
     } else {
         iframe.style.width = "0px";
     }
